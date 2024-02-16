1984f7d76e5cb003c91cffc695a5d8c429be1c63
Plamena Petkova <pl.petkova@gmail.com>
2024-02-16 11:52:17 +0200

    Change Mongo connect place

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const developerRoute = require("./src/routes/developerRoutes")
-
+const developerRoute = require("./src/routes/developerRoutes");
 
 const app = express();
 
 require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api", developerRoute);
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port:${process.env.PORT}`);
+});
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -16,17 +23,5 @@ mongoose
   .catch((e) => {
     console.log("Error", e);
   });
-
-
-app.use(express.json());
-app.use(cors());
-app.use("/api", developerRoute);
-
-
-const server = app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port:${process.env.PORT}`);
-  });
-
-
 
 
